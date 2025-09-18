823eac45fe8dcc558132c7755aa61a2a61d25dd2
Юлия (Джулия) <sixquestions666@gmail.com>
2025-09-18 14:38:02 +0300

    Upgrade app.py

--- app.py
+++ app.py
@@ -2,24 +2,26 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# Глобальный список для хранения всех пользователей (временно, для демо)
+users = []
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    user_data = None
     if request.method == 'POST':
-
+        # Получаем данные из формы
         name = request.form.get('name')
         age = request.form.get('age')
         city = request.form.get('city')
         hobby = request.form.get('hobby')
 
-        user_data = {
+        # Добавляем нового пользователя в список
+        users.append({
             'name': name,
             'age': age,
             'city': city,
             'hobby': hobby
-        }
-
-    return render_template('index.html', user_data=user_data)
+        })
+    return render_template('index.html', users=users)
 
 if __name__ == '__main__':
     app.run(debug=True)
